459b748b716171c977473614db9e9c48dd401b0c
hareeshbhuma <hareeshbhuma@gmail.com>
2019-10-11 16:11:47 -0500

    commit

--- calculatebmi.js
+++ calculatebmi.js
@@ -1,6 +1,7 @@
 function calculateBmi() {
     var weight = document.bmiForm.weight.value
     var height = document.bmiForm.height.value
+    updateConversion(); 
     if(weight > 0 && height > 0){	
     var finalBmi = weight/(height/100*height/100)
     document.bmiForm.bmi.value = finalBmi
@@ -17,4 +18,24 @@ function calculateBmi() {
     else{
     alert("Please Fill in everything correctly")
     }
+    document.querySelector('#times').innerHTML = 1;
     }
+
+    document.querySelector('#calculate').addEventListener('click', calculate);
+
+
+
+let conversions = parseInt(localStorage.setItem('conversions', '0'));
+if (isNaN(conversions)){
+    conversions = 0;
+    console.log("in if statement");
+    localStorage.setItem('conversions', conversions);
+}
+
+
+function updateConversion() {
+    conversions = parseInt(conversions) + 1;
+    console.log(conversions);
+    localStorage.setItem('conversions', conversions);
+    document.querySelector('#times').innerHTML = conversions;
+}
